cc3d694c0c5babf364b130c3f5cafd0a14700ad1
Toan Pham <phamvantoan1993@gmail.com>
2018-10-05 22:52:12 +0700

    First Commit

---
+++ js/model_emotion.js
@@ -0,0 +1,28 @@
+var MODEL_EMOTION;
+var IS_MODEL_EMOTION_LOADED = false;
+initEmotion = async () => {
+  MODEL_EMOTION = await tf.loadModel("./model/emotion/model.json");
+
+  //Warm up network
+  MODEL_EMOTION.predict(tf.zeros([1, 64, 64, 1]));
+  IS_MODEL_EMOTION_LOADED = true;
+   toastr.info('Model Emotion Loaded.')
+};
+
+initEmotion();
+
+function predictEmotion(input) {
+  var startTime = performance.now();
+
+  var r = MODEL_EMOTION.predict(input);
+  var result = r.dataSync();
+  var tresult = tf.tensor(result)
+  var label_index = tf.argMax(tresult).dataSync()[0]
+
+  var label_percent = result[label_index].toFixed(4) * 100;
+
+  var totalTime = performance.now() - startTime;
+  //console.log('Predict Emotion Time: ' + Math.floor(totalTime));
+
+  return {"result": result, "label": LABEL_EMOTIONS[label_index], "percent": label_percent};
+}
